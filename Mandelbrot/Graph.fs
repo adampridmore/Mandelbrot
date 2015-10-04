8d735a1995d771f90ce14defdca7f19e66dc4e94
Adam Pridmore <adam_pridmore@hotmail.com>
2015-10-04 19:52:32 +0100

    - More changes.

---
+++ Mandelbrot/Graph.fs
@@ -0,0 +1,16 @@
+﻿module Graph
+open System.Drawing
+
+type Graph(width:int, height:int, minX:double, maxX:double, minY:double, maxY:double) =
+    let bitmap = new Bitmap(width, height)
+    
+    member this.Width = width
+    member this.Height = height
+    member this.MinX = minX
+    member this.MinY = minY
+    member this.MaxY = maxY
+    member this.Bitmap = bitmap
+    member this.DrawLine point = 
+        use graphics = Graphics.FromImage(this.Bitmap)
+        let p = new Pen(new SolidBrush(Color.Red),5.f);
+        graphics.DrawLine(p, 0,0, 100, 100)
--- Mandelbrot/Program.fs
+++ Mandelbrot/Program.fs
@@ -1,7 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System.Windows.Forms
+open System.Drawing
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let graph = new Graph.Graph(300, 200,0.,100.,0.,100.)
+  
+    let picutureBox = new PictureBox()
+    picutureBox.Dock <- DockStyle.Fill
+  
+    let graph = new Graph.Graph(100,100,0.,0.,100.,100.);
+
+    graph.DrawLine ()
+    picutureBox.Image <- graph.Bitmap
+
+    picutureBox.BorderStyle <- BorderStyle.Fixed3D
+
+    let form = new Form()
+    form.Controls.Add(picutureBox)
+
+    form.ShowDialog() |> ignore
+          
+    0
