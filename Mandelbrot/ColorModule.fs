7c88ffe3cafad5a22ed66ff088f112a350048edd
Adam Pridmore <adam_pridmore@hotmail.com>
2017-09-23 22:02:50 +0100

    - Fixed performance by calculating the colors onece.

--- Mandelbrot/ColorModule.fs
+++ Mandelbrot/ColorModule.fs
@@ -2,6 +2,28 @@
 
 open System.Drawing
 
+let private namesColors2 = 
+    [|
+        (66, 30, 15);
+        (25, 7, 26);
+        (9, 1, 47);
+        (4, 4, 73);
+        (0, 7, 100);
+        (12, 44, 138);
+        (24, 82, 177);
+        (57, 125, 209);
+        (134, 181, 229);
+        (211, 236, 248);
+        (241, 233, 191);
+        (248, 201, 95);
+        (255, 170, 0);
+        (204, 128, 0);
+        (153, 87, 0);
+        (106, 52, 3);
+    |]
+    |> Seq.map (fun (r,g,b) -> Color.FromArgb(255, r, g, b) )
+    |> Seq.toArray
+
 let toColor iterations i = 
 //    let colors = [|
 //        Color.Blue;
@@ -39,26 +61,9 @@ let toColor iterations i =
 //        |> Seq.sortBy (fun c -> c.B + c.G + c.R)
 //        |> Seq.toArray
 
-    let namesColors2 = 
-        [|
-            (66, 30, 15);
-            (25, 7, 26);
-            (9, 1, 47);
-            (4, 4, 73);
-            (0, 7, 100);
-            (12, 44, 138);
-            (24, 82, 177);
-            (57, 125, 209);
-            (134, 181, 229);
-            (211, 236, 248);
-            (241, 233, 191);
-            (248, 201, 95);
-            (255, 170, 0);
-            (204, 128, 0);
-            (153, 87, 0);
-            (106, 52, 3);
-        |]
-        |> Seq.map (fun (r,g,b) -> Color.FromArgb(255, r, g, b) )
+
+
+
 //        |> Seq.rev
 
 
@@ -67,7 +72,7 @@ let toColor iterations i =
 //        |> Seq.map (fun i -> Color.FromArgb(150, 150, i))
 //        |> Seq.toArray
         
-    let colors = namesColors2 |> Seq.toArray
+    let colors = namesColors2 
 
     //colors.[i % colors.Length]
 
