5434472841783915f9e650dbfceb465e682d1d0a
Adam Pridmore <adam_pridmore@hotmail.com>
2017-09-30 22:11:19 +0100

    - Added a checkbox to the UI to maintain the aspect ratio.

--- MandelbrotWpf/MainWindow.xaml.cs
+++ MandelbrotWpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -194,19 +195,38 @@ namespace MandelbrotWpf
 
             var mousePosition = e.GetPosition(canvas);
 
+            var ratio = canvas.ActualHeight/canvas.ActualWidth;
+
             var x1 = _startOfDragPosition.Value.X;
             var y1 = _startOfDragPosition.Value.Y;
             var x2 = mousePosition.X;
             var y2 = mousePosition.Y;
 
+            var dragWidth = mousePosition.X - _startOfDragPosition.Value.X;
+
+            var dragHeight = GetDragHeight(dragWidth, ratio, y2, y1);
+
             _dragRectangle.Stroke = SystemColors.WindowFrameBrush;
-            _dragRectangle.Width = Math.Abs(x2 - x1);
-            _dragRectangle.Height = Math.Abs(y2 - y1);
+
+            _dragRectangle.Width = Math.Abs(dragWidth);
+            _dragRectangle.Height = Math.Abs(dragHeight);
 
             _dragRectangle.SetValue(Canvas.LeftProperty, Min(x1, x2));
             _dragRectangle.SetValue(Canvas.TopProperty, Min(y1, y2));
 
             _dragRectangle.Visibility = Visibility.Visible;
+        }
+
+        private double GetDragHeight(double dragWidth, double ratio, double y2, double y1)
+        {
+            if (cbMaintainAspectRation.IsChecked.Value)
+            {
+                return dragWidth*ratio;
+            }
+            else
+            {
+                return _dragRectangle.Height = Math.Abs(y2 - y1);
+            }
         }
 
         private void bClearSelection_Click(object sender, RoutedEventArgs e)
