bb38563cf67615d7f71e177404dde1fd4dc6549e
Adam Pridmore <adam_pridmore@hotmail.com>
2020-03-12 21:19:23 +0000

    - Migrated code from original to 2

--- MandelbrotWeb2/Controllers/HomeController.cs
+++ MandelbrotWeb2/Controllers/HomeController.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using MandelbrotWeb2.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace MandelbrotWeb2.Controllers
+namespace MandelbrotWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // GET: Home
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ MandelbrotWeb2/Controllers/MapTileController.cs
@@ -0,0 +1,87 @@
+﻿using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Mandelbrot;
+using Repository;
+using Repository.Domain;
+
+namespace MandelbrotWeb.Controllers
+{
+    public class MapTileController : Controller
+    {
+        public const string TileLabelsSetName = "TileLabels";
+        private readonly TileRepository _tileRepository = Create();
+
+        public static TileRepository Create()
+        {
+            // TODO: Read from config
+            //var mongoUri = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
+            var mongoUri = "mongodb://localhost/tiles";
+
+            return new TileRepository(mongoUri);
+        }
+
+        // Generates labled tiles.
+        private FileResult LabeledTiles(int x, int y, int z)
+        {
+            var tileImage = CreatePlainLabeledTile(x, y, z);
+
+            var memoryStream = new MemoryStream();
+
+            var imageFormat = ImageFormat.Png;
+            tileImage.Save(memoryStream, imageFormat);
+
+            memoryStream.Position = 0;
+
+            var contentType = $"image/{imageFormat.ToString().ToLowerInvariant()}";
+            return new FileStreamResult(memoryStream, contentType);
+        }
+
+        public ActionResult Index(string x, string y, string z, string tileSetName)
+        {
+            if (string.IsNullOrWhiteSpace(tileSetName))
+            {
+                tileSetName = Tile.DefaultSetName;
+            }
+
+            var xVal = int.Parse(x);
+            var yVal = int.Parse(y);
+            var zoom = int.Parse(z);
+
+            if (tileSetName == TileLabelsSetName)
+            {
+                return LabeledTiles(xVal, yVal, zoom);
+            }
+
+            return LoadFromDb(tileSetName, xVal, yVal, zoom);
+        }
+
+        private ActionResult LoadFromDb(string tileSetName, int x, int y, int zoom)
+        {
+            var tile = LoadTile(x, y, zoom, tileSetName);
+            if (tile == null)
+            {
+                return NotFound();
+            }
+
+            var imageFormat = ImageFormat.Png;
+
+            var memoryStream = new MemoryStream(tile);
+
+            var contentType = $"image/{imageFormat.ToString().ToLowerInvariant()}";
+            return new FileStreamResult(memoryStream, contentType);
+        }
+
+        private byte[] LoadTile(int x, int y, int zoom, string tileSetName)
+        {
+            return MapTileGenerator.getTileImageByte(x, y, zoom, tileSetName, _tileRepository);
+        }
+
+        private static Image CreatePlainLabeledTile(int x, int y, int zoom)
+        {
+            return TileGenerator.generateTile(x, y, zoom);
+        }
+    }
+}
