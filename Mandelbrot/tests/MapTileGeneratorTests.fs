3577b707246b9f671635375cce34b258dcab5ba5
Adam Pridmore <adam_pridmore@hotmail.com>
2020-03-24 21:32:19 +0000

    - Added a renderer performance test.

---
+++ Mandelbrot/tests/MapTileGeneratorTests.fs
@@ -0,0 +1,38 @@
+module Mandelbrot.MapTileGeneratorTests
+
+open NUnit.Framework
+open FsUnit
+open Mandelbrot.MandelbrotCalculator
+open System.Numerics
+open MapTileGenerator
+
+let iterationsToCheck = 100
+    
+let size = System.Drawing.Size(512,512) 
+
+let viewPort = {
+    XMin = -0.001
+    XMax =  0.001
+    YMin = -0.001
+    YMax =  0.001
+}
+
+let testPrint (s:string) = TestContext.Progress.WriteLine(s)
+
+let graph = Graph(size.Width, size.Height, viewPort, iterationsToCheck)
+
+let performance (name : string) (fn: (Unit -> Unit) ) : System.TimeSpan =
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    fn() |> ignore
+    stopwatch.Stop()
+   
+    testPrint(sprintf "Duration : %A" (stopwatch.Elapsed))
+   
+    stopwatch.Elapsed
+  
+    
+[<Test>]
+let ``Performance 2``()=    
+    
+    performance "test1" (fun () -> graph |> renderSet iterationsToCheck) 
+    |> ignore
