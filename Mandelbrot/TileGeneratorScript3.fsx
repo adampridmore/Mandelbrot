264a7b6e1831a14e9126139f9cb33671be139a20
adampridmore <adam_pridmore@hotmail.com>
2021-08-03 22:57:36 +0100

    Generate numberphile image

--- Mandelbrot/TileGeneratorScript3.fsx
+++ Mandelbrot/TileGeneratorScript3.fsx
@@ -26,30 +26,53 @@ open System.Configuration
     
 #time "on"
 
-let filename = "Mandelbrot_1.png"
+// let filename = "Mandelbrot_1.png"
 
 // let zoom = 1
 // let tile = {X=0;Y=0;Filename=("tile1.png");Zoom=zoom}
 // let imageBytes = getTileImageByte (tile.X, tile.Y, tile.Zoom, tilesetName, repository)
 // System.IO.File.WriteAllBytes(path, imageBytes)
 
-let real = -1.63149737002
-let imaginary = 1.985e-8
-let zoom = 1.0 / 1.0
+// let iterations = 400
+let iterations = 10000
+// let imageResolution = (2.0**12.0) |> int32 // 4096
+// let imageResolution = 1024
+let imageResolution = 4096 * 2
 
-let viewPort = { 
-    RectangleD.XMin = -1.0
-    XMax = 1.0
-    YMin = -1.0
-    YMax = 1.0
-}
-let size = new System.Drawing.Size(100,100)
-let iterations = 400
+let renderImage index span = 
+    let real = -1.63149737002
+    let imaginary = 1.985e-8
+    
+    // let span = 1.0 / zoom
+    let filename = sprintf "Images/Mandelbrot_%d.png" index
 
-let graph = Graph(size.Width, size.Height, viewPort, iterations)
+    let viewPort = {
+        RectangleD.XMin = real - span
+        XMax = real + span
+        YMin = imaginary - (span * 0.8)
+        YMax = imaginary + (span * 0.8)
+    }
 
-let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+    let size = System.Drawing.Size(imageResolution,((imageResolution |> float) * 0.8) |> int32)
 
-graph |> renderSet iterations
+    let graph = Graph(size.Width, size.Height, viewPort, iterations)
 
-graph.Bitmap.Save(filename)
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+
+    graph |> renderSet iterations
+
+    graph.Bitmap.Save(filename)
+
+
+// let logSequence = Seq.map(fun x -> printfn "%A" x; x)
+
+// let zoomFactor = 0.8
+let zoomFactor = 0.75
+
+// seq{1..100}
+[27]
+|> Seq.map(fun i -> i, zoomFactor ** (i |> float))
+|> Seq.map(fun x -> printfn "%A" x; x)
+// |> Seq.iter (ignore)
+|> Seq.iter (fun (i, span) -> renderImage i span)
+// |> Seq.iter (printf "%f\n")
